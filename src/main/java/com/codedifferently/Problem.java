99b3015d2c02c61504edf33e68ef3b7d03e6a83a
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-09-22 12:26:10 -0400

    updates

--- src/main/java/com/codedifferently/Problem.java
+++ src/main/java/com/codedifferently/Problem.java
@@ -2,6 +2,21 @@ package com.codedifferently;
 
 public class Problem {
     public String reverseAString(String input){
-        return null;
+        String reverseInput = "";
+        for(int x = input.length()-1; x >= 0; x--){
+            reverseInput +=  input.charAt(x);
+        }
+        // reversed String
+
+        String[] words = reverseInput.split(" ");
+        String response = "";
+        for(String word:words){
+            word = word.toLowerCase();
+            String firstLetter = word.substring(0,1).toUpperCase();
+            String restOfLetters = word.substring(1);
+            String fixedWord = firstLetter + restOfLetters;
+            response += fixedWord + " ";
+        }
+        return response.trim();
     }
 }
