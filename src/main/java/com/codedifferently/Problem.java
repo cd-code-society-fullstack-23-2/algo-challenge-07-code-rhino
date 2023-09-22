6971172431db07c65da22a3ec0074501d6d5145e
Tariq Hook <thook@Tariqs-MacBook-Pro-2.local>
2023-09-22 12:29:02 -0400

    remixed

--- src/main/java/com/codedifferently/Problem.java
+++ src/main/java/com/codedifferently/Problem.java
@@ -2,20 +2,12 @@ package com.codedifferently;
 
 public class Problem {
     public String reverseAString(String input){
-        String reverseInput = "";
-        for(int x = input.length()-1; x >= 0; x--){
-            reverseInput +=  input.charAt(x);
-        }
-        // reversed String
-
+        StringBuilder builder = new StringBuilder(input).reverse();
+        String reverseInput = builder.toString().toLowerCase();
         String[] words = reverseInput.split(" ");
         String response = "";
         for(String word:words){
-            word = word.toLowerCase();
-            String firstLetter = word.substring(0,1).toUpperCase();
-            String restOfLetters = word.substring(1);
-            String fixedWord = firstLetter + restOfLetters;
-            response += fixedWord + " ";
+            response += word.substring(0,1).toUpperCase() + word.substring(1) + " ";
         }
         return response.trim();
     }
